c95733cbef915ac84b0d2ab48359276862a1f1f1
26aaai <atlascode@126.com>
2025-07-27 13:27:25 +0800

    Add files via upload

---
+++ run/eval.bash
@@ -0,0 +1,16 @@
+name=eval
+
+flag="--vlnbert prevalent
+
+      --load=Downloads/ckpt/best_val
+
+      --test_only 0
+      --train valid
+      --data_dir Downloads/Data
+      --setting seen
+
+      --eval_iters -1
+      --batchSize 10
+      --maxAction 10"
+
+CUDA_VISIBLE_DEVICES=0 python r2r_src/train.py $flag --name $name
